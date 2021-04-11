b6bf6ddd4765dcea66fe935138b0eab99581da8c
Keshav Kabra <ksvkabra@gmail.com>
2021-04-11 19:28:21 +0530

    ✨ : Using decorators with stories.

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Center from '../Center/Center';
 import Button from './Button';
 
 export default {
@@ -7,6 +8,7 @@ export default {
   args: {
     children: 'Button',
   },
+  decorators: [(story) => <Center>{story()}</Center>],
 };
 
 export const Primary = () => <Button variant='primary'>Primary</Button>;
