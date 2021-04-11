ae5d8346d4204dccd741427f0055e06954400613
Keshav Kabra <ksvkabra@gmail.com>
2021-04-11 18:55:37 +0530

    Using args mechanism to create stories

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -4,9 +4,31 @@ import Button from './Button';
 export default {
   title: 'Form/Button',
   component: Button,
+  args: {
+    children: 'Button',
+  },
 };
 
 export const Primary = () => <Button variant='primary'>Primary</Button>;
 export const Success = () => <Button variant='success'>Success</Button>;
 export const Danger = () => <Button variant='danger'>Danger</Button>;
 export const Secondary = () => <Button variant='secondary'>Secondary</Button>;
+
+const Template = (args) => <Button {...args} />;
+
+export const PrimaryA = Template.bind({});
+PrimaryA.args = {
+  variant: 'primary',
+};
+
+export const SecondaryA = Template.bind({});
+SecondaryA.args = {
+  variant: 'secondary',
+  children: 'Seconday Args',
+};
+
+export const LongPrimaryA = Template.bind({});
+LongPrimaryA.args = {
+  ...PrimaryA.args,
+  children: 'Long Primary Args',
+};
